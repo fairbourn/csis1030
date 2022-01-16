430f82769c8e0adcaa047b11186d2869e02cadf5
fairbourn <77865030+fairbourn@users.noreply.github.com>
2022-01-15 18:34:28 -0700

    Add files via upload

---
+++ SQL/query2.sql
@@ -0,0 +1,3 @@
+SELECT first_name, last_name, billing_address_1, primary_phone
+FROM customers
+WHERE billing_state = "OH";
---
+++ assignment 2/2. Write a Select query that will show your name, address, phone.sql
@@ -0,0 +1,4 @@
+SELECT first_name, last_name, primary_phone, billing_address_1
+FROM customers
+WHERE ccid = "1";
+
